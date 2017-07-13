14bfeadb7a8e2bf2e8e49eeb491e877590a0ebaa
ARM32 <arm32.x@gmail.com>
2017-07-13 13:12:16 -0400

    Start using Git

---
+++ app/src/main/java/arm32/android/nfclibrary/YourTagsFragment.java
@@ -0,0 +1,20 @@
+package arm32.android.nfclibrary;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class YourTagsFragment extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.page_your_tags, container, false);
+        return view;
+    }
+}
